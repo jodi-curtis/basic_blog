657a8c8e52f8d79b138da4a70d55170c2ee7f3b1
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2024-09-05 16:52:11 +0100

    Changed database URL to render postgres

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ import os
 SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
 # When deploying, set in the environment to the PostgreSQL URL
-connection_string = "postgresql://postgres:JojoPG99@localhost:5432/analysis"
+#connection_string = "postgresql://postgres:JojoPG99@localhost:5432/analysis"
+connection_string = "postgresql://jodi:lGtZlAMYChZLjKHNrSCS92ujMnbjO6pM@dpg-crcskc3qf0us73as8o7g-a.oregon-postgres.render.com/test_h0tm"
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', connection_string)
